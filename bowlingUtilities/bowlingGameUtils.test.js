2725dfe6bc03f38035e32273d3a94527320c8c3e
Sukhacoder02 <sukhmansindhipb05@gmail.com>
2023-01-18 12:44:35 +0530

    test: implement tests for getBestScore function

--- bowlingUtilities/bowlingGameUtils.test.js
+++ bowlingUtilities/bowlingGameUtils.test.js
@@ -7,14 +7,11 @@ const games = [
     [6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
 describe('Bowling Game Utilities:-',() => {
-    // describe('getBestScore function:- ',() => {
-    //     it('Should get the best score when input is a 2d array of numbers',() => {
-    //         expect(getBestScore(games)).toEqual([90,30,16]);
-    //     });
-    //     it('Should throw an error when input is not a 2d array',() => {
-    //         expect(()=>getScore('avc')).toThrow('Input is not a 2d array');
-    //     })
-    // });
+    describe('getBestScore function:- ',() => {
+        it('Should get the best score when input is a 2d array of numbers',() => {
+            expect(getBestScore(games)).toBe(90);
+        });
+    });
     describe('getScore function:-', () => {
         it('Should get the score of the game when input is a array of numbers',() => {
             expect(getScore(games[0])).toBe(90);
